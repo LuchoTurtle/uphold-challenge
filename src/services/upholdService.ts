319f6ddd10989c1e8ebbc24b6b2469ff0c282656
Luís Arteiro <lucho14garfield@gmail.com>
2025-05-15 15:46:06 +0100

    feat: Adding fetching tickets service.

---
+++ src/services/upholdService.ts
@@ -0,0 +1,26 @@
+import SDK from "@uphold/uphold-sdk-javascript";
+
+const sdk = new SDK({
+  baseUrl: import.meta.env.DEV ? "/api" : "http://api-sandbox.uphold.com",  // proxy to avoid CORS issues (check vite.config.js)
+  clientId: "foo",
+  clientSecret: "bar",
+});
+
+const ratesCache = new Map();
+
+export const getCurrencyRates = async (baseCurrency = "") => {
+  if (ratesCache.has(baseCurrency)) {
+    return ratesCache.get(baseCurrency);
+  }
+
+  try {
+    const rates = await sdk.getTicker(baseCurrency);
+
+    ratesCache.set(baseCurrency, rates);
+
+    return rates;
+  } catch (error) {
+    console.error("Error fetching currency rates:", error);
+    throw error;
+  }
+};
